cf6090aa23cb7c45f78e36e11bdff055810bc764
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-03-31 16:34:53 -0600

    ugh

--- Service/ValuesService.js
+++ Service/ValuesService.js
@@ -5,21 +5,20 @@ import { valuesService } from "../Service/ValuesService.js";
 //Private
 function _draw() {
   let values = ProxyState.values;
-  let template = `<div class="row" id="houses">
-  <!-- <div class="col-md-4 mb-3">
-      <div class="card shadow">
-          <img class="card-img-top" src="//placehold.it/200x200" alt="">
-          <div class="card-body">
-              <h4 class="card-title">ROOMS | BATHROOMS | ADDRESS</h4>
-              <p class="card-text">SQUARE FEET - PRICE</p>
-          </div>
-          <div class="px-3 pb-3 d-flex justify-content-between">
-              <button type="button" class="btn btn-danger">Delete</button>
-              <button type="button" class="btn btn-info">Bid</button>
-          </div>
+  let template = `<div class="col-md-4 mb-3">
+  <div class="card shadow">
+      <img class="card-img-top" src="${this.imgUrl}" alt="">
+      <div class="card-body">
+          <h4 class="card-title">${this.rooms} | ${this.bathrooms} | ${this.address}</h4>
+          <p class="card-text">${this.sqfoot} - $${this.price.toFixed(2)}</p>
       </div>
-  </div> 
-</div>`
+      <div class="px-3 pb-3 d-flex justify-content-between">
+          <button type="button" class="btn btn-danger" onclick="app.carsController.deleteCar('${this.id}')">Delete</button>
+          <button type="button" class="btn btn-info" onclick="app.carsController.bid('${this.id}')">Bid</button>
+      </div>
+  </div>
+</div>
+`
   values.forEach(v => template += v.Template)
   document.getElementById("app").innerHTML = /*html*/`
   <button className="btn btn-info" onclick="app.valuesController.addValue()">Add Value</button>  
